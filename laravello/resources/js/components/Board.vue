b921eb830b6d59041d0bcdf285b2737eea9a2cc2
alinit <dumitru.mihailalin@gmail.com>
2022-04-19 01:25:39 +0300

    update trello

---
+++ laravello/resources/js/components/Board.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Board Component</div>
+
+                    <div class="card-body">
+                       <h2>This is Board component </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ laravello/resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="{{mix('css/app.css')}}">
+        <script src="{{mix('js/app.js')}}" defer></script>
+    </head>
+    <body>
+        <div id="app">
+            <div class="col-md-6">
+                <Board>
+            </div>
+            <div class="col-md-6">
+                <Dashboard>
+            </div>
+        </div>
+    </body>
+</html>
